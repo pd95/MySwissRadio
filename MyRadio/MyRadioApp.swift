18f340b5120505dce634276575154e9706c6089a
Philipp <pd95@users.noreply.github.com>
2020-10-06 09:37:58 +0200

    Initial Commit

---
+++ MyRadio/MyRadioApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyRadioApp.swift
+//  MyRadio
+//
+//  Created by Philipp on 06.10.20.
+//
+
+import SwiftUI
+
+@main
+struct MyRadioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
