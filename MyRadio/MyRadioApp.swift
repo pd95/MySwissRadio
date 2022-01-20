8173450086bd2c3c74985d2daeeb0c33e3bc9456
Philipp <pd95@users.noreply.github.com>
2022-01-20 17:56:12 +0100

    Add debugging helper: allow creating a backup of the current settings and apply some custom settings to simulate problems.

--- MyRadio/MyRadioApp.swift
+++ MyRadio/MyRadioApp.swift
@@ -16,6 +16,11 @@ class AppDelegate: NSObject, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil) -> Bool {
         logger.log("ApplicationDelegate didFinishLaunchingWithOptions. \(String(describing: launchOptions?.keys))")
 
+        #if DEBUG
+        backupSettings()
+        // authconfigapplySettings()
+        #endif
+
 // It is unclear whether we really have to request Siri authorization. Everything works fine so far without it!?
 //        INPreferences.requestSiriAuthorization { (authStatus: INSiriAuthorizationStatus) in
 //            self.logger.log("SiriAutorization: \(String(describing: authStatus))")
@@ -28,6 +33,46 @@ class AppDelegate: NSObject, UIApplicationDelegate {
         logger.log("ApplicationDelegate handlerFor intent: \(intent)")
         return MyRadioModel.main
     }
+
+#if DEBUG
+    private func backupSettings() {
+        let date = ISO8601DateFormatter().string(from: Date())
+            .replacingOccurrences(of: ":", with: "")
+        dumpUserDefaults(UserDefaults.standard, filename: "userdefaults-standard-\(date)")
+        dumpUserDefaults(UserDefaults.shared, filename: "userdefaults-shared-\(date)")
+        dumpUserDefaults(UserDefaults(suiteName: "")!, filename: "userdefaults-authconfig-\(date)")
+    }
+
+    private func applySettings() {
+        SettingsStore.shared.lastLivestreamRefreshDate = Date().addingTimeInterval(-31*24*60*60)  // expired!
+
+        let response = OAuthenticator.AccessTokenResponse(
+            accessToken: "ThisIsAnInvalidToken",
+            tokenType: "Bearer",
+            expiresIn: nil,
+            refreshToken: nil,
+            scope: nil
+        )
+        let encoder = JSONEncoder()
+        encoder.keyEncodingStrategy = .convertToSnakeCase
+        let data = try? encoder.encode(response)
+        UserDefaults(suiteName: "")!.set(data ?? Data(), forKey: "AuthData")
+    }
+
+    private func dumpUserDefaults(_ defaults: UserDefaults, filename: String) {
+        do {
+            let dict = defaults.dictionaryRepresentation()
+            if let data = try? PropertyListSerialization.data(fromPropertyList: dict, format: .xml, options: 0) {
+                let url = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0]
+                    .appendingPathComponent("\(filename).plist")
+                try data.write(to: url)
+                logger.log("dumpUserDefaults: \(url)")
+            }
+        } catch {
+            logger.log("dumpUserDefaults: \(error.localizedDescription)")
+        }
+    }
+#endif
 }
 
 @main
